ef87d66be25e6aab60198bb30f90b5babd99044e
zacharytq <zacharytq@gmail.com>
2021-05-25 10:23:29 -0400

    store connected to app

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './reducers/rootReducer';
 
-export default createStore(
+const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 )
+
+export default store;
 
