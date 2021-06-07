139546ba8bbca2c8a6faf9f7227607bd32150cec
Zachary Quinn <zacharytq@gmail.com>
2021-06-06 22:03:51 -0400

    usersSlice worked on signupUser

---
+++ src/auth/Signup.js
@@ -0,0 +1,12 @@
+import  { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+const Signup = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  return (
+     
+  )
+}
--- src/store/usersSlice.js
+++ src/store/usersSlice.js
@@ -1,3 +1,5 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
 export const setToken = (token) => {
   localStorage.setItem('token', token);
   localStorage.setItem('lastLoginTime', new Date(Date.now()).getTime());
@@ -12,6 +14,35 @@ export const getToken = () => {
   }
 }
 
+export const signupUser = (credentials) => {
+  return async (dispatch) => {
+    return fetch('http://127.0.0.1:3001/signup',{
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          setToken(resp.headers.get('Authorization'))
+          return resp.json()
+        } else {
+          throw Error(resp.json())
+        }
+      })
+      .then((json) => {
+        dispatch({ type: 'users/createUser', payload: json.data })
+      })
+      .catch((error) => {
+        dispatch({ type: 'users/createUserError', payload: error.status.message })
+      })
+  }
+}
+
+const fetchCurrentUser = createAsyncThunk('users/fetchCurrentUser')
+
 const initialState = {
   status: 'idle',
   loggedIn: false,
@@ -22,7 +53,15 @@ const initialState = {
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {},
+  reducers: {
+    createUser(state, action) {
+      state.currentUser = action.payload
+      state.loggedIn = true
+    },
+    createUserError(state, action) {
+      state.error = action.payload
+    }
+  },
   extraReducers: {
     [fetchCurrentUser.pending]: (state) => {
       state.status = 'loading'
